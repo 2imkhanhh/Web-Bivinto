59b8d33e5df0b32ff994384c076886d879e5e309
khanhh <namkhanhle3110@gmail.com>
2026-07-06 22:35:51 +0700

    update view

--- web-bivinto/resources/js/Pages/Admin/InventoryDetail.vue
+++ web-bivinto/resources/js/Pages/Admin/InventoryDetail.vue
@@ -244,3 +244,16 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+/* Chrome, Safari, Edge, Opera */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+/* Firefox */
+input[type=number] {
+  -moz-appearance: textfield;
+}
+</style>
